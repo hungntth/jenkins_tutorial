6273452bac1ece53c0130f72c4e5af85ef2b068e
hungntth <1119thaihungnguyen@gmail.com>
2023-03-17 11:41:40 +0700

    test action

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/hello', (req, res) => {
+    res.json('hello')
+})
+
 app.listen(3000, () => {
     console.log('app listen port 3000')
 })
